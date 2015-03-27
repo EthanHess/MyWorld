d5180afdb7f0b8751fa04ad05f964c30f76c052e
Ethan Hess <ech1988@gmail.com>
2015-03-27 16:37:02 -0600

    adds stack
    Signed-off-by: Ethan Hess <ech1988@gmail.com>

--- MyWorld/MainViewController.m
+++ MyWorld/MainViewController.m
@@ -17,8 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.mapView = [[MKMapView alloc]initWithFrame:CGRectMake(15, 100, self.view.frame.size.width - 30, 200)];
+    self.mapView = [[MKMapView alloc]initWithFrame:CGRectMake(15, 150, self.view.frame.size.width - 30, 300)];
     [self.view addSubview:self.mapView];
+    
+    self.welcomeLabel = [[UILabel alloc]initWithFrame:CGRectMake(50, 80, self.view.frame.size.width - 100, 45)];
+    self.welcomeLabel.font = [UIFont systemFontOfSize:32];
+    self.welcomeLabel.text = @" Welcome to My World! ";
+    self.welcomeLabel.backgroundColor = [UIColor lightGrayColor];
+    [self.welcomeLabel sizeToFit];
+    [self.view addSubview:self.welcomeLabel];
+    
     
 }
 
